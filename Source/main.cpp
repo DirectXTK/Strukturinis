7ade0f03fd703bd2e0facb0b9e722772f0de1357
Vytas Jasiūnas <s057595@ad.viko.lt>
2024-09-12 10:59:34 +0300

    Darbas

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,41 @@
 #include<iostream>
+#include <cstdint>
+void UZD06() {
 
-int main(){
+  int a,b,sum{};
 
-  std::cout << "O";
-  //Comments
+  std::cout <<"Uzduotis 06\nParasykite du dvizenklius skaicius\n";
+
+  std::cin >>a >>b;
+  sum +=(a%10+b%10)+(a/10+b/10);
+  std::cout << "Skaitmenu suma="<<sum;
+
+
+
+}
+void UZD05() {
+  std::cout <<"Uzduotis 05\nParasykite 5 skaicius\n";
+
+int a1,a2,a3,a4,a5{};
+  std::cin >> a1>> a2>>a3>>a4>>a5;
+  std::cout <<"Penkiu Skaiciu vidurkis="<< (float)(a1+a2+a3+a4+a5)/5.f;
+
+
+}
+int main() {
+  uint32_t NR{};
+  std::cout <<"Parasykute uzduoties numeri\n";
+  std::cin >> NR;
+  switch(NR) {
+    case 5: {
+      UZD05();
+      break;
+    }
+    case 6: {
+      UZD06();
+      break;
+    }
+  }
+
   return 0;
 }
